0c24a9447d764f9aa33e2b9a15d57761551dea68
Manjunath Davanam <manjunathd@ilimi.in>
2020-07-07 15:17:26 +0530

    Issue SC-1810 fix: Testcase fixes

--- course-aggregate-updater/src/test/scala/org/sunbird/async/fixture/EventFixture.scala
+++ course-aggregate-updater/src/test/scala/org/sunbird/async/fixture/EventFixture.scala
@@ -173,4 +173,8 @@ object EventFixture {
 
   //===QUERY====BEGIN BATCH UPDATE sunbird_courses.activity_user_agg SET agg=agg+{'progress':2},agg_last_updated=agg_last_updated+{'progress':1593612416135} WHERE activity_id='unit11' AND activity_type='course-unit' AND context_id='cb:B11' AND user_id='U11';UPDATE sunbird_courses.activity_user_agg SET agg=agg+{'progress':1},agg_last_updated=agg_last_updated+{'progress':1593612416079} WHERE activity_id='unit22' AND activity_type='course-unit' AND context_id='cb:B11' AND user_id='U11';UPDATE sunbird_courses.activity_user_agg SET agg=agg+{'progress':2},agg_last_updated=agg_last_updated+{'progress':1593612416145} WHERE activity_id='C11' AND activity_type='course' AND context_id='cb:B11' AND user_id='U11';APPLY BATCH;
 
+  val EVENT_4: String =
+    """
+      |{"eid":"BE_JOB_REQUEST","ets":1563788371969,"mid":"LMS.1563788371969.590c5fa0-0ce8-46ed-bf6c-681c0a1fdac8","actor":{"type":"System","id":"Course Batch Updater"},"context":{"pdata":{"ver":"1.0","id":"org.sunbird.platform"}},"object":{"type":"CourseBatchEnrolment","id":"0126083288437637121_8454cb21-3ce9-4e30-85b5-fade097880d8"},"edata":{"contents":[{"contentId":"do_11260735471149056012299","status":2},{"contentId":"do_11260735471149056012300","status":1},{"contentId":"do_11260735471149056012301","status":1}],"action":"batch-enrolment-update","iteration":1,"batchId":"0126083288437637121","userId":"8454cb21-3ce9-4e30-85b5-fade097880d8","courseId":"do_1127212344324751361295"}}
+      |""".stripMargin
 }
