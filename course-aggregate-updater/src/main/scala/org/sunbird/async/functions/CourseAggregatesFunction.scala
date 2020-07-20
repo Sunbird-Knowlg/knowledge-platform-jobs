4198ee7748c1c94e8a2a6902df2725e9af3b24a3
Manjunath Davanam <manjunathd@ilimi.in>
2020-07-20 21:12:51 +0530

    Issue #000 fix: Triggering a process function when the events reaches the threshold.

--- course-aggregate-updater/src/main/scala/org/sunbird/async/functions/CourseAggregatesFunction.scala
+++ course-aggregate-updater/src/main/scala/org/sunbird/async/functions/CourseAggregatesFunction.scala
@@ -238,7 +238,6 @@ class CourseAggregatesFunction(config: CourseAggregateUpdaterConfig)(implicit va
           selectWhere.and(QueryBuilder.eq(col._1, col._2))
       }
     })
-   // logger.info("DB Read" + selectWhere.toString)
     metrics.incCounter(config.dbReadCount)
     cassandraUtil.find(selectWhere.toString).asScala.toList
 
@@ -254,22 +253,19 @@ class CourseAggregatesFunction(config: CourseAggregateUpdaterConfig)(implicit va
       queries.map(query => cqlBatch.add(query))
       val result = cassandraUtil.upsert(cqlBatch.toString)
       if (result) {
-       // logger.info("DataBase Update got success" + cqlBatch.toString)
         metrics.incCounter(config.successEventCount)
         metrics.incCounter(config.dbUpdateCount)
       } else {
-       // logger.info("Database update has failed" + cqlBatch.toString)
+        logger.info("Database update has failed" + cqlBatch.toString)
       }
     })
   }
 
   def readFromCache(key: String, metrics: Metrics): List[String] = {
-    //logger.info("redis key" + key)
-    //logger.info("redis db" + cache.dbIndex)
     metrics.incCounter(config.cacheHitCount)
     val list = cache.getKeyMembers(key)
-    //if (CollectionUtils.isEmpty(list))
-      //logger.info("Redis cache (smembers) not available for key: " + key)
+    if (CollectionUtils.isEmpty(list))
+      logger.info("Redis cache (smembers) not available for key: " + key)
     list.asScala.toList
   }
 
