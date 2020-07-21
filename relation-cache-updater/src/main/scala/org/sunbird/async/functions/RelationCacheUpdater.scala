202d3ba37a0c4780d40dae6117552ef649c50998
Manjunath Davanam <manjunathd@ilimi.in>
2020-07-21 10:35:21 +0530

    Issue #000 fix: Renamed relation-cache-updater job the package name

---
+++ relation-cache-updater/src/main/scala/org/sunbird/async/functions/RelationCacheUpdater.scala
@@ -0,0 +1,196 @@
+package org.sunbird.async.functions
+
+import java.lang.reflect.Type
+
+import com.datastax.driver.core.querybuilder.QueryBuilder
+import com.google.gson.reflect.TypeToken
+import org.apache.commons.collections.{CollectionUtils, MapUtils}
+import org.apache.commons.lang3.StringUtils
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.slf4j.LoggerFactory
+import org.sunbird.async.core.cache.{DataCache, RedisConnect}
+import org.sunbird.async.core.job.{BaseProcessFunction, Metrics}
+import org.sunbird.async.core.util.CassandraUtil
+import org.sunbird.async.task.RelationCacheUpdaterConfig
+
+import scala.collection.JavaConverters._
+
+
+class RelationCacheUpdater(config: RelationCacheUpdaterConfig)
+                          (implicit val stringTypeInfo: TypeInformation[String],
+                           @transient var cassandraUtil: CassandraUtil = null)
+    extends BaseProcessFunction[java.util.Map[String, AnyRef], String](config) {
+
+    private[this] val logger = LoggerFactory.getLogger(classOf[RelationCacheUpdater])
+    val mapType: Type = new TypeToken[java.util.Map[String, AnyRef]]() {}.getType
+    private var dataCache: DataCache = _
+    private var collectionCache: DataCache = _
+    lazy private val mapper: ObjectMapper = new ObjectMapper()
+
+
+    override def open(parameters: Configuration): Unit = {
+        super.open(parameters)
+        cassandraUtil = new CassandraUtil(config.dbHost, config.dbPort)
+        val redisConnect = new RedisConnect(config)
+        dataCache = new DataCache(config, redisConnect, config.relationCacheStore, List())
+        collectionCache = new DataCache(config, redisConnect, config.collectionCacheStore, List())
+        dataCache.init()
+        collectionCache.init()
+    }
+
+    override def close(): Unit = {
+        cassandraUtil.close()
+        dataCache.close()
+        collectionCache.close()
+        super.close()
+    }
+
+    override def processElement(event: java.util.Map[String, AnyRef], context: ProcessFunction[java.util.Map[String, AnyRef], String]#Context, metrics: Metrics): Unit = {
+        val eData = event.get("edata").asInstanceOf[java.util.Map[String, AnyRef]]
+        if (isValidEvent(eData)) {
+            val rootId = eData.get("id").asInstanceOf[String]
+            logger.info("Processing - identifier: " + rootId)
+            val hierarchy = getHierarchy(rootId)
+            if (MapUtils.isNotEmpty(hierarchy)) {
+                val leafNodesMap = getLeafNodes(rootId, hierarchy)
+                logger.info("Leaf-nodes cache updating for: " + leafNodesMap.size)
+                storeDataInCache(rootId, "leafnodes", leafNodesMap, dataCache)
+                val ancestorsMap = getAncestors(rootId, hierarchy)
+                logger.info("Ancestors cache updating for: "+ ancestorsMap.size)
+                storeDataInCache(rootId, "ancestors", ancestorsMap, dataCache)
+                val unitsMap = getUnitMaps(hierarchy)
+                logger.info("Units cache updating for: "+ unitsMap.size)
+                storeDataInCache("", "", unitsMap, collectionCache)
+                metrics.incCounter(config.successEventCount)
+            } else {
+                logger.warn("Hierarchy Empty: " + rootId)
+                metrics.incCounter(config.skippedEventCount)
+            }
+        } else {
+            metrics.incCounter(config.skippedEventCount)
+        }
+        metrics.incCounter(config.totalEventsCount)
+    }
+
+    override def metricsList(): List[String] = {
+        List(config.successEventCount, config.failedEventCount, config.skippedEventCount, config.totalEventsCount)
+    }
+
+    private def isValidEvent(eData: java.util.Map[String, AnyRef]): Boolean = {
+        val action = eData.getOrDefault("action", "").asInstanceOf[String]
+        val mimeType = eData.getOrDefault("mimeType", "").asInstanceOf[String]
+        val identifier = eData.getOrDefault("id", "").asInstanceOf[String]
+
+        StringUtils.equalsIgnoreCase(action, "publish-shallow-content") &&
+            StringUtils.equalsIgnoreCase(mimeType, "application/vnd.ekstep.content-collection") &&
+            StringUtils.isNotBlank(identifier)
+    }
+
+    private def getHierarchy(identifier: String): java.util.Map[String, AnyRef] = {
+        val hierarchy = readHierarchyFromDb(identifier)
+        if (StringUtils.isNotBlank(hierarchy))
+            mapper.readValue(hierarchy, classOf[java.util.Map[String, AnyRef]])
+        else new java.util.HashMap[String, AnyRef]()
+    }
+
+    private def getLeafNodes(identifier: String, hierarchy: java.util.Map[String, AnyRef]): Map[String, List[String]] = {
+        val mimeType = hierarchy.getOrDefault("mimeType", "").asInstanceOf[String]
+        val leafNodesMap = if (StringUtils.equalsIgnoreCase(mimeType, "application/vnd.ekstep.content-collection")) {
+            val leafNodes = getOrComposeLeafNodes(hierarchy, false)
+            val map: Map[String, List[String]] = if (leafNodes.nonEmpty) Map() + (identifier -> leafNodes) else Map()
+            val children = getChildren(hierarchy)
+            val childLeafNodesMap = if (CollectionUtils.isNotEmpty(children)) {
+                children.asScala.map(child => {
+                    val childId = child.get("identifier").asInstanceOf[String]
+                    getLeafNodes(childId, child)
+                }).flatten.toMap
+            } else Map()
+            map ++ childLeafNodesMap
+        } else Map()
+        leafNodesMap.filter(m => m._2.nonEmpty).toMap
+    }
+
+    private def getOrComposeLeafNodes(hierarchy: java.util.Map[String, AnyRef], compose: Boolean = true): List[String] = {
+        if (hierarchy.containsKey("leafNodes") && !compose)
+            hierarchy.getOrDefault("leafNodes", java.util.Arrays.asList()).asInstanceOf[java.util.List[String]].asScala.toList
+        else {
+            val children = getChildren(hierarchy)
+            val childCollections = children.asScala.filter(c => isCollection(c))
+            val leafList = childCollections.map(coll => getOrComposeLeafNodes(coll, true)).flatten.toList
+            val ids = children.asScala.filterNot(c => isCollection(c)).map(c => c.getOrDefault("identifier", "").asInstanceOf[String]).filter(id => StringUtils.isNotBlank(id))
+            leafList ++ ids
+        }
+    }
+
+    private def isCollection(content: java.util.Map[String, AnyRef]): Boolean = {
+        StringUtils.equalsIgnoreCase(content.getOrDefault("mimeType", "").asInstanceOf[String], "application/vnd.ekstep.content-collection")
+    }
+
+    private def getAncestors(identifier: String, hierarchy: java.util.Map[String, AnyRef], parents: List[String] = List()): Map[String, List[String]] = {
+        val mimeType = hierarchy.getOrDefault("mimeType", "").asInstanceOf[String]
+        val isCollection = (StringUtils.equalsIgnoreCase(mimeType, "application/vnd.ekstep.content-collection"))
+        val ancestors = if (isCollection) identifier :: parents else parents
+        val ancestorsMap = if (isCollection) {
+            getChildren(hierarchy).asScala.map(child => {
+                val childId = child.get("identifier").asInstanceOf[String]
+                getAncestors(childId, child, ancestors)
+            }).filter(m => m.nonEmpty).reduceOption((a,b) => {
+                // Here we are merging the Resource ancestors where it is used multiple times - Functional.
+                // convert maps to seq, to keep duplicate keys and concat then group by key - Code explanation.
+                val grouped = (a.toSeq ++ b.toSeq).groupBy(_._1)
+                grouped.mapValues(_.map(_._2).toList.flatten.distinct)
+            }).getOrElse(Map())
+        } else {
+            Map(identifier -> parents)
+        }
+        ancestorsMap.filter(m => m._2.nonEmpty)
+    }
+
+    private def getChildren(hierarchy: java.util.Map[String, AnyRef]) = {
+        val children = hierarchy.getOrDefault("children", java.util.Arrays.asList()).asInstanceOf[java.util.List[java.util.Map[String, AnyRef]]]
+        if (CollectionUtils.isEmpty(children)) List().asJava else children
+    }
+
+    private def storeDataInCache(rootId: String, suffix: String, dataMap: Map[String, AnyRef], cache: DataCache) = {
+        val finalSuffix = if (StringUtils.isNotBlank(suffix)) ":" + suffix else ""
+        val finalPrefix = if (StringUtils.isNoneBlank(rootId)) rootId + ":" else ""
+        try {
+            dataMap.foreach(each => each._2 match {
+                case value: List[String] => cache.addListWithRetry(finalPrefix + each._1 + finalSuffix, each._2.asInstanceOf[List[String]])
+                case _ =>  cache.setWithRetry(finalPrefix + each._1 + finalSuffix, each._2.asInstanceOf[String])
+            })
+        } catch {
+            case e: Throwable => {
+                println("Failed to write data for " + suffix + ": " + rootId + " with map: " + dataMap)
+                throw e
+            }
+        }
+    }
+
+    def readHierarchyFromDb(identifier: String): String = {
+        val columnName = "hierarchy"
+        val selectQuery = QueryBuilder.select().column(columnName).from(config.dbKeyspace, config.dbTable)
+        selectQuery.where.and(QueryBuilder.eq(config.hierarchyPrimaryKey.head, identifier))
+        val rows = cassandraUtil.find(selectQuery.toString)
+        if (CollectionUtils.isNotEmpty(rows))
+            rows.asScala.head.getObject("hierarchy").asInstanceOf[String]
+        else
+            ""
+    }
+
+    private def getUnitMaps(hierarchy: java.util.Map[String, AnyRef]): Map[String, String] = {
+        val mimeType = hierarchy.getOrDefault("mimeType", "").asInstanceOf[String]
+        if (StringUtils.equalsIgnoreCase(mimeType, "application/vnd.ekstep.content-collection")) {
+            val children = getChildren(hierarchy).asScala
+            // TODO - Here the collection with "visibility:Parent" constructed with children in it. May be we should remove children.
+            if (children.nonEmpty)
+                (if (StringUtils.equalsIgnoreCase(hierarchy.getOrDefault("visibility", "").asInstanceOf[String], "Parent"))
+                    Map(hierarchy.get("identifier").asInstanceOf[String] -> mapper.writeValueAsString(hierarchy))
+                else Map()) ++ children.flatMap(child => getUnitMaps(child)).toMap
+            else Map()
+        } else Map()
+    }
+}
---
+++ relation-cache-updater/src/test/scala/org/sunbird/async/fixture/EventFixture.scala
@@ -0,0 +1,13 @@
+package org.sunbird.async.fixture
+
+object EventFixture {
+
+  val EVENT_1: String =
+    """
+      |{"actor":{"id":"Post Publish Processor", "type":"System"}, "eid":"BE_JOB_REQUEST", "edata":{"createdFor":["ORG_001"], "createdBy":"95e4942d-cbe8-477d-aebd-ad8e6de4bfc8", "name":"Untitled Course", "action":"publish-shallow-content", "iteration":1.0, "id":"do_11305855864948326411234", "mimeType":"application/vnd.ekstep.content-collection", "contentType":"Course", "pkgVersion":7.0, "status":"Live"}, "partition":0, "ets":"1.593769627322E12", "context":{"pdata":{"ver":1.0, "id":"org.ekstep.platform"}, "channel":"b00bc992ef25f1a9a8d63291e20efc8d", "env":"sunbirddev"}, "mid":"LP.1593769627322.459a018c-5ec3-4c11-96c1-cd84d3786b85", "object":{"ver":"1593769626118", "id":"do_11305855864948326411234"}}
+      |""".stripMargin
+  val EVENT_2: String =
+    """
+      |{"actor":{"id":"Post Publish Processor", "type":"System"}, "eid":"BE_JOB_REQUEST", "edata":{"createdFor":["ORG_001"], "createdBy":"95e4942d-cbe8-477d-aebd-ad8e6de4bfc8", "name":"Untitled Course", "action":"publish-shallow-content", "iteration":1.0, "id":"KP_FT_1594149835504", "mimeType":"application/vnd.ekstep.content-collection", "contentType":"Course", "pkgVersion":7.0, "status":"Live"}, "partition":0, "ets":"1.593769627322E12", "context":{"pdata":{"ver":1.0, "id":"org.ekstep.platform"}, "channel":"b00bc992ef25f1a9a8d63291e20efc8d", "env":"sunbirddev"}, "mid":"LP.1593769627322.459a018c-5ec3-4c11-96c1-cd84d3786b85", "object":{"ver":"1593769626118", "id":"KP_FT_1594149835504"}}
+      |""".stripMargin
+}
