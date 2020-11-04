d90440e37e35d75c3fe5744fc59dd8e9936a2695
NehaVerma <neha0305verma@gmail.com>
2020-11-04 17:44:40 +0530

    certificate generator public key change

--- certificate-generator/src/main/scala/org/sunbird/job/CertMapper.scala
+++ certificate-generator/src/main/scala/org/sunbird/job/CertMapper.scala
@@ -84,14 +84,15 @@ class CertMapper(properties: java.util.Map[String, String]) {
     var publicKeys = issuerPublicKeys
     if (CollectionUtils.isEmpty(issuerPublicKeys) && MapUtils.isNotEmpty(keys)) {
       publicKeys = new java.util.ArrayList[String]
+      publicKeys.add(keys.get(JsonKey.ID).asInstanceOf[String])
     }
     val validatedPublicKeys: java.util.List[String] = new java.util.ArrayList[String]
     if (CollectionUtils.isNotEmpty(publicKeys))
       publicKeys.forEach((publicKey: String) => {
-      if (!publicKey.startsWith("http"))
-        validatedPublicKeys.add(properties.get(JsonKey.BASE_PATH).concat("/") + JsonKey.KEYS.concat("/") + publicKey.concat("_publicKey.json"))
-      else validatedPublicKeys.add(publicKey)
-    })
+        if (!publicKey.startsWith("http"))
+          validatedPublicKeys.add(properties.get(JsonKey.BASE_PATH).concat("/") + JsonKey.KEYS.concat("/") + publicKey.concat("_publicKey.json"))
+        else validatedPublicKeys.add(publicKey)
+      })
     validatedPublicKeys
   }
 
