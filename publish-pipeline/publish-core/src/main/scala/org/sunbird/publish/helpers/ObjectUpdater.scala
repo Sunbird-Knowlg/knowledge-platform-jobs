132438a84fa7ba5aaf35d10b092839a6c4c1367c
Mahesh Kumar Gangula <maheshkumargangula@users.noreply.github.com>
2021-01-05 23:20:59 +0530

    Issue #000 fix: update configurations and queries.

--- publish-pipeline/publish-core/src/main/scala/org/sunbird/publish/helpers/ObjectUpdater.scala
+++ publish-pipeline/publish-core/src/main/scala/org/sunbird/publish/helpers/ObjectUpdater.scala
@@ -12,20 +12,22 @@ trait ObjectUpdater {
 
   private[this] val logger = LoggerFactory.getLogger(classOf[ObjectUpdater])
 
+  @throws[Exception]
   def saveOnSuccess(obj: ObjectData, extSaveFn: (ObjectData) => Unit)(implicit neo4JUtil: Neo4JUtil): Unit = {
     val publishType = obj.metadata.getOrElse("publish_type", "Public").asInstanceOf[String]
     val status = if (StringUtils.equals("Private", publishType)) "Unlisted" else "Live"
     val nodeId = obj.metadata.get("IL_UNIQUE_ID").get
-    val query = s"""MATCH (n:domain{IL_UNIQUE_ID:"$nodeId"}) SET n.status="$status" $auditPropsUpdateQuery();"""
+    val query = s"""MATCH (n:domain{IL_UNIQUE_ID:"$nodeId"}) SET n.status="$status" $auditPropsUpdateQuery;"""
     logger.info("Query: " + query)
     neo4JUtil.executeQuery(query)
     extSaveFn(obj)
   }
 
+  @throws[Exception]
   def saveOnFailure(obj: ObjectData, messages: List[String])(implicit neo4JUtil: Neo4JUtil): Unit = {
     val errorMessages = messages.mkString("; ")
     val nodeId = obj.metadata.get("IL_UNIQUE_ID").get
-    val query = s"""MATCH (n:domain{IL_UNIQUE_ID:"$nodeId"}) SET n.status="Failed", n.publishError="$errorMessages" $auditPropsUpdateQuery();"""
+    val query = s"""MATCH (n:domain{IL_UNIQUE_ID:"$nodeId"}) SET n.status="Failed", n.publishError="$errorMessages" $auditPropsUpdateQuery;"""
     logger.info("Query: " + query)
     neo4JUtil.executeQuery(query)
   }
@@ -33,7 +35,7 @@ trait ObjectUpdater {
   private def auditPropsUpdateQuery(): String = {
     val sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
     val updatedOn = sdf.format(new Date())
-    s""" n.lastUpdatedOn=$updatedOn """
+    s""", n.lastUpdatedOn="$updatedOn""""
   }
 
 }
