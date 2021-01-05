4225c9f364de405116feffd07419d9d025bd7239
Pradyumna Nagendra <pradyumnan@ilimi.in>
2021-01-05 14:07:22 +0530

    Removed certificate related old flink jobs

--- cert-processor/src/main/java/org/sunbird/incredible/pojos/ob/HostedVerification.java
+++
@@ -1,8 +0,0 @@
-package org.sunbird.incredible.pojos.ob;
-
-public class HostedVerification extends VerificationObject {
-    public HostedVerification() {
-        String[] type = new String[] {"HostedBadge"};
-        setType(type);
-    }
-}
--- post-certificate-processor/src/main/scala/org/sunbird/job/domain/FailedEvent.scala
+++
@@ -1,5 +0,0 @@
-package org.sunbird.job.domain
-
-
-case class FailedEvent(errorCode: String,
-                       error: String)
