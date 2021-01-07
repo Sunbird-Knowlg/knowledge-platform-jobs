2629d5df9de1d604cd8b0218cf07bd56ecd1def6
Kumar Gauraw <er.krgauraw@gmail.com>
2021-01-07 16:42:08 +0530

    Issue #SC-2181 feat: added test case

--- publish-pipeline/publish-core/src/test/scala/org/sunbird/publish/spec/FrameworkDataEnrichmentTestSpec.scala
+++ publish-pipeline/publish-core/src/test/scala/org/sunbird/publish/spec/FrameworkDataEnrichmentTestSpec.scala
@@ -28,7 +28,6 @@ class FrameworkDataEnrichmentTestSpec extends FlatSpec with BeforeAndAfterAll wi
 		when(mockNeo4JUtil.getNodesName(ArgumentMatchers.anyObject())).thenReturn(Map[String, String]("ncf_medium_telugu" -> "Telugu", "ncf_medium_english" -> "English",  "ncf_board_cbse" -> "CBSE", "ncfcopy_board_ncert" -> "NCERT"))
 		val obj = new TestFrameworkDataEnrichment()
 		val result = obj.enrichFrameworkData(data)
-		println("result meta "+result.metadata)
 		result.metadata.getOrElse("se_mediumIds", List()).asInstanceOf[List[String]] should have length(2)
 		result.metadata.getOrElse("se_mediumIds", List()).asInstanceOf[List[String]].contains("ncf_medium_telugu") should be (true)
 		result.metadata.getOrElse("se_boardIds", List()).asInstanceOf[List[String]] should have length(2)
@@ -39,6 +38,20 @@ class FrameworkDataEnrichmentTestSpec extends FlatSpec with BeforeAndAfterAll wi
 		result.metadata.getOrElse("se_mediums", List()).asInstanceOf[List[String]].contains("English") should be (true)
 		result.metadata.getOrElse("se_boards", List()).asInstanceOf[List[String]] should have length(2)
 		result.metadata.getOrElse("se_boards", List()).asInstanceOf[List[String]].contains("CBSE") should be (true)
+	}
+
+	"enrichFrameworkData with only targetFramework" should "enrich only se_FWIds" in {
+		val data = new ObjectData("do_123", Map[String, AnyRef]("name" -> "Content Name", "identifier" -> "do_123", "IL_UNIQUE_ID" -> "do_123", "pkgVersion" -> 0.0.asInstanceOf[AnyRef], "framework" -> "NCF", "targetFWIds" -> List("TPD", "NCFCOPY").asJava))
+		when(mockNeo4JUtil.getNodesName(ArgumentMatchers.anyObject())).thenReturn(Map[String, String]("ncf_medium_telugu" -> "Telugu", "ncf_medium_english" -> "English",  "ncf_board_cbse" -> "CBSE", "ncfcopy_board_ncert" -> "NCERT"))
+		val obj = new TestFrameworkDataEnrichment()
+		val result = obj.enrichFrameworkData(data)
+		result.metadata.contains("se_mediumIds") should be (false)
+		result.metadata.contains("se_boardIds") should be (false)
+		result.metadata.contains("se_FWIds") should be (true)
+		result.metadata.contains("se_mediums") should be (false)
+		result.metadata.contains("se_boards") should be (false)
+		result.metadata.getOrElse("se_FWIds", List()).asInstanceOf[List[String]] should have length(3)
+		result.metadata.getOrElse("se_FWIds", List()).asInstanceOf[List[String]].contains("TPD") should be (true)
 	}
 }
 
