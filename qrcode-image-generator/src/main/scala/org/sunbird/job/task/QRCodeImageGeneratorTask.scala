10c079ff1ddd428846c9127e41d4f9b1d21213fd
Utkarsha Kapoor <utkit14@gmail.com>
2021-05-10 11:55:10 +0530

    SC-2276: Removed unnecessary code

--- qrcode-image-generator/src/main/scala/org/sunbird/job/task/QRCodeImageGeneratorTask.scala
+++ qrcode-image-generator/src/main/scala/org/sunbird/job/task/QRCodeImageGeneratorTask.scala
@@ -8,16 +8,14 @@ import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.java.typeutils.TypeExtractor
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.sunbird.job.audithistory.domain.Event
 import org.sunbird.job.connector.FlinkKafkaConnector
-import org.sunbird.job.functions.{ QRCodeImageGeneratorFunction}
+import org.sunbird.job.functions.QRCodeImageGeneratorFunction
 import org.sunbird.job.util.{ElasticSearchUtil, FlinkUtil}
 
 
 class QRCodeImageGeneratorTask(config: QRCodeImageGeneratorConfig, kafkaConnector: FlinkKafkaConnector) {
   def process(): Unit = {
     implicit val env: StreamExecutionEnvironment = FlinkUtil.getExecutionContext(config)
-    implicit val eventTypeInfo: TypeInformation[Event] = TypeExtractor.getForClass(classOf[Event])
     implicit val mapTypeInfo: TypeInformation[util.Map[String, AnyRef]] = TypeExtractor.getForClass(classOf[util.Map[String, AnyRef]])
     implicit val stringTypeInfo: TypeInformation[String] = TypeExtractor.getForClass(classOf[String])
 
