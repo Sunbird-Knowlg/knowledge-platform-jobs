8fd4c22f2046c763a8e47cd3eb37ee5170f4eb09
Manjunath Davanam <>
2021-05-17 11:46:04 +0530

    Issue SC-1914 fix: audit history flink job package name changes.

---
+++ audit-history-indexer/src/main/scala/org/sunbird/job/audithistory/functions/AuditHistoryIndexer.scala
@@ -0,0 +1,42 @@
+package org.sunbird.job.audithistory.functions
+
+import java.util
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.sunbird.job.audithistory.domain.Event
+import org.sunbird.job.service.AuditHistoryIndexerService
+import org.sunbird.job.task.AuditHistoryIndexerConfig
+import org.sunbird.job.{BaseProcessFunction, Metrics}
+import org.sunbird.job.util.ElasticSearchUtil
+
+class AuditHistoryIndexer(config: AuditHistoryIndexerConfig, var esUtil: ElasticSearchUtil)
+                          (implicit mapTypeInfo: TypeInformation[util.Map[String, Any]],
+                           stringTypeInfo: TypeInformation[String])
+                          extends BaseProcessFunction[Event, String](config) with AuditHistoryIndexerService{
+
+    override def metricsList(): List[String] = {
+        List(config.totalEventsCount, config.successEventCount, config.failedEventCount, config.esFailedEventCount, config.skippedEventCount)
+    }
+
+    override def open(parameters: Configuration): Unit = {
+        super.open(parameters)
+        if (esUtil == null) {
+            esUtil = new ElasticSearchUtil(config.esConnectionInfo, config.auditHistoryIndex, config.auditHistoryIndexType)
+        }
+    }
+
+    override def close(): Unit = {
+        esUtil.close()
+        super.close()
+    }
+
+    override def processElement(event: Event,
+                                context: ProcessFunction[Event, String]#Context,
+                                metrics: Metrics): Unit = {
+        metrics.incCounter(config.totalEventsCount)
+        if(event.isValid) {
+            processEvent(event, metrics)(esUtil, config)
+        } else metrics.incCounter(config.skippedEventCount)
+    }
+}
