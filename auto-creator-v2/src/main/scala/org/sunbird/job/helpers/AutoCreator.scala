76d70734c933c66945c5882f1b60b64fe3e8df65
Mahesh Kumar Gangula <maheshkumargangula@users.noreply.github.com>
2021-05-24 15:30:55 +0530

    Issue #000 fix: compilation fix.

--- auto-creator-v2/src/main/scala/org/sunbird/job/helpers/AutoCreator.scala
+++ auto-creator-v2/src/main/scala/org/sunbird/job/helpers/AutoCreator.scala
@@ -41,9 +41,10 @@ trait AutoCreator extends ObjectUpdater with CollectionUpdater with HierarchyEnr
       .find(p => StringUtils.equalsIgnoreCase(identifier, p.getOrElse("identifier", "").asInstanceOf[String])).getOrElse(Map())
     if (metaUrl.nonEmpty) {
       // TODO: deprecate setting "origin" after single sourcing refactoring.
-      val originDetails = Map("origin" -> identifier, "originData" -> s"{\"identifier\": \"$identifier\",\"repository\":\"${metaUrl.head}\"}")
-      val metadata = getMetaUrlData(metaUrl.head, objectType)(httpUtil)
-      manifestMetadata.++(metadata).++(originDetails)
+      val originData = "{\"identifier\": \"" + identifier + "\",\"repository\":\"" + metaUrl.head + "\"}"
+      val originDetails = Map[String, AnyRef]("origin" -> identifier, "originData" -> originData)
+      val metadata = getMetaUrlData(metaUrl.head, objectType)(httpUtil) ++ originDetails
+      manifestMetadata.++(metadata)
     } else manifestMetadata
   }
 
