a1598b628a62398484e1ed70c26f7d8b58229d39
amitpriyadarshi <darshi.1983@gmail.com>
2021-06-10 00:43:01 +0530

    Issue #SB-24310 feat: Adding framework master category update scripts and resetting path of other scripts

--- publish-pipeline/publish-core/src/main/scala/org/sunbird/job/publish/helpers/ObjectEnrichment.scala
+++ publish-pipeline/publish-core/src/main/scala/org/sunbird/job/publish/helpers/ObjectEnrichment.scala
@@ -1,5 +1,6 @@
 package org.sunbird.job.publish.helpers
 
+import org.sunbird.job.publish.config.PublishConfig
 import org.sunbird.job.publish.core.{ExtDataConfig, ObjectData}
 import org.sunbird.job.publish.util.CloudStorageUtil
 import org.sunbird.job.util.{CassandraUtil, Neo4JUtil}
@@ -7,7 +8,7 @@ import org.sunbird.job.util.{CassandraUtil, Neo4JUtil}
 trait ObjectEnrichment extends FrameworkDataEnrichment with ThumbnailGenerator {
 
 
-  def enrichObject(obj: ObjectData)(implicit neo4JUtil: Neo4JUtil, cassandraUtil: CassandraUtil, readerConfig: ExtDataConfig, cloudStorageUtil: CloudStorageUtil): ObjectData = {
+  def enrichObject(obj: ObjectData)(implicit neo4JUtil: Neo4JUtil, cassandraUtil: CassandraUtil, readerConfig: ExtDataConfig, cloudStorageUtil: CloudStorageUtil, config: PublishConfig): ObjectData = {
     val newObj = enrichFrameworkData(obj)
     val enObj = enrichObjectMetadata(newObj).getOrElse(newObj)
     generateThumbnail(enObj).getOrElse(enObj)
--- publish-pipeline/questionset-publish/src/main/scala/org/sunbird/job/questionset/function/QuestionPublishFunction.scala
+++ publish-pipeline/questionset-publish/src/main/scala/org/sunbird/job/questionset/function/QuestionPublishFunction.scala
@@ -59,7 +59,7 @@ class QuestionPublishFunction(config: QuestionSetPublishConfig, httpUtil: HttpUt
 		val obj = getObject(data.identifier, data.pkgVersion, readerConfig)(neo4JUtil, cassandraUtil)
 		val messages:List[String] = validate(obj, obj.identifier, validateQuestion)
 		if (messages.isEmpty) {
-			val enrichedObj = enrichObject(obj)(neo4JUtil, cassandraUtil, readerConfig, cloudStorageUtil)
+			val enrichedObj = enrichObject(obj)(neo4JUtil, cassandraUtil, readerConfig, cloudStorageUtil, config)
 			val objWithEcar = getObjectWithEcar(enrichedObj, pkgTypes)(ec, cloudStorageUtil, definitionCache, definitionConfig)
 			logger.info("Ecar generation done for Question: " + objWithEcar.identifier)
 			saveOnSuccess(objWithEcar)(neo4JUtil, cassandraUtil, readerConfig, definitionCache, definitionConfig)
