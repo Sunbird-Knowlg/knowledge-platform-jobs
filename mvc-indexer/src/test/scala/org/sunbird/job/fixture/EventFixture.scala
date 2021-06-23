34a340583ea8945f055b045fd3882fe8d15c0278
kumarks1122 <kumar.kandasamy@tarento.com>
2021-06-23 11:24:23 +0530

    Issue #SC-2085 | Test cases updated

--- mvc-indexer/src/test/scala/org/sunbird/job/fixture/EventFixture.scala
+++ mvc-indexer/src/test/scala/org/sunbird/job/fixture/EventFixture.scala
@@ -14,17 +14,17 @@ object EventFixture {
 
   val EVENT_3: String =
     """
-      |{"object":{"id":"do_112806963140329472124"},"index":"false","ets":"1591949601174","eventData":{"action":"update-ml-keywords","stage":"2","ml_Keywords":["maths","addition","add"],"ml_contentText":"This is the content text for addition of two numbers."}}
+      |{"object":{"id":"do_112806963140329472124"},"index":"true","ets":"1591949601174","eventData":{"action":"update-ml-keywords","stage":"2","ml_Keywords":["maths","addition","add"],"ml_contentText":"This is the content text for addition of two numbers."}}
       |""".stripMargin
 
   val EVENT_4: String =
     """
-      |{"object":{"id":"do_112806963140329472124"},"index":"false","ets":"1591949601174","eventData":{"action":"update-ml-contenttextvector","stage":3,"ml_contentTextVector":[[1.1,2,7.4,68]]}}
+      |{"object":{"id":"do_112806963140329472124"},"index":"true","ets":"1591949601174","eventData":{"action":"update-ml-contenttextvector","stage":3,"ml_contentTextVector":[[1.1,2,7.4,68]]}}
       |""".stripMargin
 
   val EVENT_5: String =
     """
-      |{"object":{"id":"do_112806963140329472124"},"index":"false","ets":"1591949601174","eventData":{"action":"update-content-rating","stage":4,"metadata":{"me_averageRating":"1","me_total_time_spent_in_app":"2","me_total_time_spent_in_portal":"3","me_total_time_spent_in_desktop":"4","me_total_play_sessions_in_app":"5","me_total_play_sessions_in_portal":"6","me_total_play_sessions_in_desktop":"7"}}}
+      |{"object":{"id":"do_112806963140329472124"},"index":"true","ets":"1591949601174","eventData":{"action":"update-content-rating","stage":4,"metadata":{"me_averageRating":"1","me_total_time_spent_in_app":"2","me_total_time_spent_in_portal":"3","me_total_time_spent_in_desktop":"4","me_total_play_sessions_in_app":"5","me_total_play_sessions_in_portal":"6","me_total_play_sessions_in_desktop":"7"}}}
       |""".stripMargin
 
 }
