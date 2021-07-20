671399782e5681d4dea9db3a7326fdd5b8676b14
Isha Wakankar <iwakankar@hotmail.com>
2021-07-20 15:46:33 +0530

    Added test cases for code coverage

--- metrics-data-transformer/src/test/scala/org/sunbird/job/spec/service/MetricsDataTransformerServiceTestSpec.scala
+++ metrics-data-transformer/src/test/scala/org/sunbird/job/spec/service/MetricsDataTransformerServiceTestSpec.scala
@@ -37,7 +37,7 @@ class MetricsDataTransformerServiceTestSpec extends BaseTestSpec {
     super.afterAll()
   }
 
-  "MetricsDataTransformerService" should "execute" in {
+  "MetricsDataTransformerService" should "execute for valid event" in {
     val inputEvent: Event = new Event(JSONUtil.deserialize[util.Map[String, Any]](EventFixture.EVENT_1),0, 10)
     val map = new ConcurrentHashMap[String, AtomicLong]()
     map.put("success-events-count", new AtomicLong(0))
@@ -60,4 +60,43 @@ class MetricsDataTransformerServiceTestSpec extends BaseTestSpec {
     }
   }
 
+  "MetricsDataTransformerService" should "throw an exception if updating fails" in {
+    val inputEvent: Event = new Event(JSONUtil.deserialize[util.Map[String, Any]](EventFixture.EVENT_1),0, 10)
+    val map = new ConcurrentHashMap[String, AtomicLong]()
+    map.put("failed-events-count", new AtomicLong(0))
+    val metrics: Metrics = Metrics(map)
+
+    val metricList = Array("me_totalRatingsCount")
+    implicit val mockHttpUtil = mock[HttpUtil](Mockito.withSettings().serializable())
+    when(mockHttpUtil.get(anyString(), any())).thenReturn(HTTPResponse(200, """{"id":"api.v3.read.get","ver":"1.0","ts":"2020-08-07T15:56:47ZZ","params":{"resmsgid":"bbd98348-c70b-47bc-b9f1-396c5e803436","msgid":null,"err":null,"status":"successful","errmsg":null},"responseCode":"OK","result":{"content":{"parent":"do_123","origin":"do_119831368202241","mediaType":"content","name":"APSWREIS-TGT2-Day5","identifier":"do_234","description":"Enter description for Collection","resourceType":"Collection","mimeType":"application/vnd.ekstep.content-collection","contentType":"Collection","language":["English"],"objectType":"Content","status":"Live","idealScreenSize":"normal","contentEncoding":"gzip","osId":"org.ekstep.quiz.app","contentDisposition":"inline","childNodes":["do_345"],"visibility":"Default","pkgVersion":2,"idealScreenDensity":"hdpi"}}}"""))
+    implicit val config = jobConfig
+    the [Exception] thrownBy {
+      metricsDataTransformer.processEvent(inputEvent, metrics, metricList)
+    } should have message "Error writing metrics for sourcing id: do_119831368202241"
+
+    when(mockHttpUtil.patch(anyString(), any(), any())).thenReturn(HTTPResponse(404, """{"id":"api.content.hierarchy.get","ver":"3.0","ts":"2020-08-07T15:56:47ZZ","params":{"resmsgid":"bbd98348-c70b-47bc-b9f1-396c5e803436","msgid":null,"err":null,"status":"successful","errmsg":null},"responseCode":"OK","result":{"content":{}}}"""))
+    the [Exception] thrownBy {
+      metricsDataTransformer.processEvent(inputEvent, metrics, metricList)
+    } should have message "Error writing metrics for sourcing id: do_119831368202241"
+
+  }
+
+  "MetricsDataTransformerService" should "skip event if sourcing id not found" in {
+    val inputEvent: Event = new Event(JSONUtil.deserialize[util.Map[String, Any]](EventFixture.EVENT_1),0, 10)
+    val map = new ConcurrentHashMap[String, AtomicLong]()
+    map.put("skipped-events-count", new AtomicLong(0))
+    val metrics: Metrics = Metrics(map)
+
+    val metricList = Array("me_totalRatingsCount")
+    implicit val mockHttpUtil = mock[HttpUtil](Mockito.withSettings().serializable())
+    when(mockHttpUtil.get(anyString(), any())).thenReturn(HTTPResponse(404, """{"id":"api.v3.read.get","ver":"1.0","ts":"2020-08-07T15:56:47ZZ","params":{"resmsgid":"bbd98348-c70b-47bc-b9f1-396c5e803436","msgid":null,"err":null,"status":"successful","errmsg":null},"responseCode":"OK","result":{"content":{}}}"""))
+
+    implicit val config = jobConfig
+    try {
+      metricsDataTransformer.processEvent(inputEvent, metrics, metricList)
+    } catch {
+      case ex: JobExecutionException =>
+        BaseMetricsReporter.gaugeMetrics(s"${jobConfig.jobName}.${jobConfig.skippedEventCount}").getValue() should be(1)
+    }
+  }
 }
