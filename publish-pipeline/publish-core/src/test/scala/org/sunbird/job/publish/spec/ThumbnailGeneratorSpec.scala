4b49e9a13e0ec3c21d7934e824e2ee0b3d11a8fa
Kumar Gauraw <er.krgauraw@gmail.com>
2021-07-23 10:40:01 +0530

    Issue #SB-25874 fix: fix for thubmnail generation

--- publish-pipeline/publish-core/src/test/scala/org/sunbird/job/publish/spec/ThumbnailGeneratorSpec.scala
+++ publish-pipeline/publish-core/src/test/scala/org/sunbird/job/publish/spec/ThumbnailGeneratorSpec.scala
@@ -25,9 +25,22 @@ class ThumbnailGeneratorSpec extends FlatSpec with BeforeAndAfterAll with Matche
   "Object Thumbnail Generator generateThumbnail" should "add the thumbnail to ObjectData" in {
 
     val hierarchy = Map("identifier" -> "do_123", "children" -> List(Map("identifier" -> "do_234", "name" -> "Children-1"), Map("identifier" -> "do_345", "name" -> "Children-2")))
-    val metadata = Map("identifier" -> "do_123", "appIcon" -> "https://dev.sunbirded.org/assets/images/sunbird_logo.png", "IL_UNIQUE_ID" -> "do_123", "IL_FUNC_OBJECT_TYPE" -> "QuestionSet", "name" -> "Test QuestionSet", "status" -> "Live")
+    val metadata = Map("identifier" -> "do_123", "appIcon" -> "https://dev.sunbirded.org/assets/images/sunbird_logo.png", "IL_UNIQUE_ID" -> "do_123", "objectType" -> "QuestionSet", "name" -> "Test QuestionSet", "status" -> "Live")
     val objData = new ObjectData("do_123", metadata, None, Some(hierarchy))
 
+    val thumbnailGenerator = new TestThumbnailGenerator()
+    val obj = thumbnailGenerator.generateThumbnail(objData)
+    val result = obj.getOrElse(objData)
+    val resultMetadata = result.metadata
+    resultMetadata.isEmpty should be(false)
+    resultMetadata.getOrElse("posterImage", "").asInstanceOf[String].isEmpty should be(false)
+    resultMetadata.getOrElse("appIcon", "").asInstanceOf[String].isEmpty should be(false)
+  }
+
+  "Object Thumbnail Generator generateThumbnail with google drive link" should "add the thumbnail to ObjectData" in {
+    val hierarchy = Map("identifier" -> "do_123", "children" -> List(Map("identifier" -> "do_234", "name" -> "Children-1"), Map("identifier" -> "do_345", "name" -> "Children-2")))
+    val metadata = Map("identifier" -> "do_123", "appIcon" -> "https://drive.google.com/uc?export=download&id=1-dFzAeSNmx1ZRn77CEntyQA-VcBE0PKg", "IL_UNIQUE_ID" -> "do_123", "objectType" -> "QuestionSet", "name" -> "Test QuestionSet", "status" -> "Live")
+    val objData = new ObjectData("do_123", metadata, None, Some(hierarchy))
     val thumbnailGenerator = new TestThumbnailGenerator()
     val obj = thumbnailGenerator.generateThumbnail(objData)
     val result = obj.getOrElse(objData)
