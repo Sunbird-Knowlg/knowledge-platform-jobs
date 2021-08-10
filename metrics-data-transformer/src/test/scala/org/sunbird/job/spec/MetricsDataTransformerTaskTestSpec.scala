10c0806b439f408b5a131a74d9438a5be712f587
Isha Wakankar <iwakankar@hotmail.com>
2021-08-10 16:16:51 +0530

    Changes for mocking http util

--- metrics-data-transformer/src/test/scala/org/sunbird/job/spec/MetricsDataTransformerTaskTestSpec.scala
+++ metrics-data-transformer/src/test/scala/org/sunbird/job/spec/MetricsDataTransformerTaskTestSpec.scala
@@ -8,13 +8,14 @@ import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration
 import org.apache.flink.streaming.api.functions.source.SourceFunction
 import org.apache.flink.streaming.api.functions.source.SourceFunction.SourceContext
 import org.apache.flink.test.util.MiniClusterWithClientResource
+import org.mockito.ArgumentMatchers.{any, anyString}
 import org.mockito.Mockito
 import org.mockito.Mockito.when
 import org.sunbird.job.connector.FlinkKafkaConnector
 import org.sunbird.job.fixture.EventFixture
 import org.sunbird.job.metricstransformer.domain.Event
 import org.sunbird.job.metricstransformer.task.{MetricsDataTransformerConfig, MetricsDataTransformerStreamTask}
-import org.sunbird.job.util.{HttpUtil, JSONUtil}
+import org.sunbird.job.util.{HTTPResponse, HttpUtil, JSONUtil}
 import org.sunbird.spec.{BaseMetricsReporter, BaseTestSpec}
 
 import java.util
@@ -32,7 +33,6 @@ class MetricsDataTransformerTaskTestSpec extends BaseTestSpec {
   val config: Config = ConfigFactory.load("test.conf")
   val jobConfig: MetricsDataTransformerConfig = new MetricsDataTransformerConfig(config)
   val httpUtil: HttpUtil = new HttpUtil()
-  val server = new MockWebServer()
 
   var currentMilliSecond = 1605816926271L
 
@@ -43,17 +43,15 @@ class MetricsDataTransformerTaskTestSpec extends BaseTestSpec {
   }
 
   "MetricsDataTransformerTaskTestSpec" should "generate event" in {
-    server.start(9200)
-    server.enqueue(new MockResponse().setHeader(
-      "Content-Type", "application/json"
-    ).setBody("""{"_index":"kp_audit_log_2018_7","_type":"ah","_id":"HLZ-1ngBtZ15DPx6ENjU","_version":1,"result":"created","_shards":{"total":2,"successful":1,"failed":0},"_seq_no":1,"_primary_term":1}"""))
 
     when(mockKafkaUtil.kafkaJobRequestSource[Event](jobConfig.kafkaInputTopic)).thenReturn(new MetricsDataTransformerMapSource)
+    implicit val mockHttpUtil = mock[HttpUtil](Mockito.withSettings().serializable())
+    when(mockHttpUtil.get(anyString(), any())).thenReturn(HTTPResponse(200, """{"id":"api.v3.read.get","ver":"1.0","ts":"2020-08-07T15:56:47ZZ","params":{"resmsgid":"bbd98348-c70b-47bc-b9f1-396c5e803436","msgid":null,"err":null,"status":"successful","errmsg":null},"responseCode":"OK","result":{"content":{"parent":"do_123","origin":"do_119831368202241","mediaType":"content","name":"APSWREIS-TGT2-Day5","identifier":"do_234","description":"Enter description for Collection","resourceType":"Collection","mimeType":"application/vnd.ekstep.content-collection","contentType":"Collection","language":["English"],"objectType":"Content","status":"Live","idealScreenSize":"normal","contentEncoding":"gzip","osId":"org.ekstep.quiz.app","contentDisposition":"inline","childNodes":["do_345"],"visibility":"Default","pkgVersion":2,"idealScreenDensity":"hdpi"}}}"""))
+    when(mockHttpUtil.patch(anyString(), any(), any())).thenReturn(HTTPResponse(200, """{"id":"api.content.hierarchy.get","ver":"3.0","ts":"2020-08-07T15:56:47ZZ","params":{"resmsgid":"bbd98348-c70b-47bc-b9f1-396c5e803436","msgid":null,"err":null,"status":"successful","errmsg":null},"responseCode":"OK","result":{"content":{"rootId":"do_123"}}}"""))
 
-    new MetricsDataTransformerStreamTask(jobConfig, mockKafkaUtil, httpUtil).process()
+    new MetricsDataTransformerStreamTask(jobConfig, mockKafkaUtil, mockHttpUtil).process()
 
     BaseMetricsReporter.gaugeMetrics(s"${jobConfig.jobName}.${jobConfig.totalEventsCount}").getValue() should be(1)
-    server.close()
   }
 
 }
