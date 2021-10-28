bfff6f28ac1276d4ab8acab8660d9508ca5e3031
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2021-10-28 21:59:52 +0530

    Issue #SB-25533 feat: Collection Publish feature

--- publish-pipeline/content-publish/src/main/scala/org/sunbird/job/content/publish/helpers/CollectionPublisher.scala
+++ publish-pipeline/content-publish/src/main/scala/org/sunbird/job/content/publish/helpers/CollectionPublisher.scala
@@ -585,7 +585,7 @@ trait CollectionPublisher extends ObjectReader with SyncMessagesGenerator with O
                     "index" -> nextLevelNode.getOrElse("index",0).asInstanceOf[AnyRef])
                 })
               } else List.empty
-             if(finalChildList != null && finalChildList.nonEmpty)  nodeMetadata.put("children", finalChildList.asInstanceOf[AnyRef])
+             nodeMetadata.put("children", finalChildList.asInstanceOf[AnyRef])
              nodeMetadata.asScala.toMap[String, AnyRef]
             }
             else {
@@ -601,7 +601,7 @@ trait CollectionPublisher extends ObjectReader with SyncMessagesGenerator with O
                     "index" -> nextLevelNode.getOrElse("index",0).asInstanceOf[AnyRef])
                 })
               } else List.empty
-             if(finalChildList != null && finalChildList.nonEmpty)  nodeMetadata.put("children", finalChildList.asInstanceOf[AnyRef])
+             nodeMetadata.put("children", finalChildList.asInstanceOf[AnyRef])
               if (nodeMetadata.getOrElse("objectType", "").asInstanceOf[String].isEmpty) {
                 nodeMetadata += ("objectType" -> "content")
               }
