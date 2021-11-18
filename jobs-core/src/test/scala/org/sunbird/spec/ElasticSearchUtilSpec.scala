dbced5e559d914c5090207789824a736cb85dd7d
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2021-11-18 13:01:47 +0530

    Issue #SB-27350 feat: Collection Publish feature

--- jobs-core/src/test/scala/org/sunbird/spec/ElasticSearchUtilSpec.scala
+++ jobs-core/src/test/scala/org/sunbird/spec/ElasticSearchUtilSpec.scala
@@ -11,6 +11,11 @@ class ElasticSearchUtilSpec extends FlatSpec with Matchers {
   val config: Config = ConfigFactory.load("base-test.conf")
   val esUtil = new ElasticSearchUtil(config.getString("es.basePath"), "compositesearch", "cs")
 
+  "isIndexExists" should "return true if index exists" in {
+    val indexExists = esUtil.isIndexExists("compositesearch")
+    println("ElasticSearchUtilSpec:: isIndexExists test:: indexExists:: " + indexExists)
+  }
+
   "addDocument" should "add document and return index" in {
     val do_113405023736512512114Json = """{"identifier":"do_113405023736512512114","graph_id":"domain","node_id":0,"collections":["do_11340502373639782416", "do_113405023736479744112"],"objectType":"Collection","nodeType":"DATA_NODE"}"""
     val documentIndexResponse: IndexResponse = esUtil.addDocument("do_113405023736512512114",do_113405023736512512114Json)
