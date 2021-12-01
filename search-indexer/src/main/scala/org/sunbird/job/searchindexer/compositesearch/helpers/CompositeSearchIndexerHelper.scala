63b4dbc1b41d93d3ddc27a8bcb69627a1169181d
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2021-12-01 09:56:06 +0530

    Issue #SB-27833 debug: ClassCastException Debug

--- search-indexer/src/main/scala/org/sunbird/job/searchindexer/compositesearch/helpers/CompositeSearchIndexerHelper.scala
+++ search-indexer/src/main/scala/org/sunbird/job/searchindexer/compositesearch/helpers/CompositeSearchIndexerHelper.scala
@@ -34,6 +34,7 @@ trait CompositeSearchIndexerHelper {
       addedProperties.foreach(property => {
         if (!definition.externalProperties.contains(property._1)) {
           val propertyNewValue: AnyRef = property._2.asInstanceOf[Map[String, AnyRef]].getOrElse("nv", null)
+          logger.info("CompositeSearchIndexerHelper:: getIndexDocument:: property._1: " + property._1 + " || propertyNewValue: " + propertyNewValue)
           if (propertyNewValue == null) indexDocument.remove(property._1) else indexDocument.put(property._1, addMetadataToDocument(property._1, propertyNewValue, nestedFields))
         }
       })
