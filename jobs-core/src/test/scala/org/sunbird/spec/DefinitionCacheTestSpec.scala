62d72fa22642dc30454d4b03c29df6eccf0f5831
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2021-12-22 12:02:50 +0530

    Issue #SB-22864 feat: relational_metadata addition

--- jobs-core/src/test/scala/org/sunbird/spec/DefinitionCacheTestSpec.scala
+++ jobs-core/src/test/scala/org/sunbird/spec/DefinitionCacheTestSpec.scala
@@ -39,6 +39,7 @@ class DefinitionCacheTestSpec extends FlatSpec with BeforeAndAfterAll with Match
   it should "return external properties"  in {
     val definition = definitionCache.getDefinition("Collection", "1.0", basePath)
     definition.externalProperties.size should be(2)
-    definition.externalProperties contains("hierarchy")
+    assert(definition.externalProperties contains("hierarchy"))
+    assert(definition.externalProperties contains("relational_metadata"))
   }
 }
