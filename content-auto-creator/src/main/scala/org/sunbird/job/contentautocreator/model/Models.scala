7023be7d1f4eab045aa78a20d28fff65c8b05d34
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2022-01-31 16:24:32 +0530

    Issue #SB-28066 feat: Content Auto-creator Flink Job

--- content-auto-creator/src/main/scala/org/sunbird/job/contentautocreator/model/Models.scala
+++
@@ -1,7 +0,0 @@
-package org.sunbird.job.contentautocreator.model
-
-case class ExtDataConfig(keyspace: String, table: String, primaryKey:List[String], propsMapping: Map[String, AnyRef])
-
-case class ObjectParent(identifier: String, parents: List[Map[String, String]])
-
-class ObjectData(val identifier: String, val objectType: String, val metadata: Map[String, AnyRef], val extData: Option[Map[String, AnyRef]] = None, val hierarchy: Option[Map[String, AnyRef]] = None)
