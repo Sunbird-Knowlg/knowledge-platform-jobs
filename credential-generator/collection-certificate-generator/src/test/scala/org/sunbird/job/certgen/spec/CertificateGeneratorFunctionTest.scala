7352734aaca45292a0aaf91e0781fd6ee3620eab
pritha-tarento <pritha.chattopadhyay@tarento.com>
2022-03-01 19:46:50 +0530

    Issue #28969 | test case added

--- credential-generator/collection-certificate-generator/src/test/scala/org/sunbird/job/certgen/spec/CertificateGeneratorFunctionTest.scala
+++ credential-generator/collection-certificate-generator/src/test/scala/org/sunbird/job/certgen/spec/CertificateGeneratorFunctionTest.scala
@@ -4,18 +4,28 @@ import com.typesafe.config.{Config, ConfigFactory}
 import org.cassandraunit.CQLDataLoader
 import org.cassandraunit.dataset.cql.FileCQLDataSet
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper
+import org.mockito.Mockito
+import org.sunbird.incredible.StorageParams
+import org.sunbird.incredible.processor.store.StorageService
+import org.sunbird.job.Metrics
 import org.sunbird.job.certgen.domain.Event
 import org.sunbird.job.certgen.fixture.EventFixture
-import org.sunbird.job.certgen.functions.CertValidator
+import org.sunbird.job.certgen.functions.{CertValidator, CertificateGeneratorFunction}
 import org.sunbird.job.certgen.task.CertificateGeneratorConfig
-import org.sunbird.job.util.{CassandraUtil, JSONUtil}
+import org.sunbird.job.util.{CassandraUtil, HttpUtil, JSONUtil}
 import org.sunbird.spec.BaseTestSpec
 
-class CertificateGeneratorFunctionTest extends BaseTestSpec{
+class CertificateGeneratorFunctionTest extends BaseTestSpec {
 
   var cassandraUtil: CassandraUtil = _
   val config: Config = ConfigFactory.load("test.conf")
   lazy val jobConfig: CertificateGeneratorConfig = new CertificateGeneratorConfig(config)
+  val httpUtil: HttpUtil = new HttpUtil
+  val mockHttpUtil:HttpUtil = mock[HttpUtil](Mockito.withSettings().serializable())
+  val storageParams: StorageParams = StorageParams(jobConfig.storageType, jobConfig.azureStorageKey, jobConfig.azureStorageSecret, jobConfig.containerName)
+  val storageService: StorageService = new StorageService(storageParams)
+  val metricJson = s"""{"${jobConfig.enrollmentDbReadCount}": 0, "${jobConfig.skippedEventCount}": 0}"""
+  val mockMetrics = mock[Metrics](Mockito.withSettings().serializable())
 
   override protected def beforeAll(): Unit = {
     super.beforeAll()
@@ -39,7 +49,7 @@ class CertificateGeneratorFunctionTest extends BaseTestSpec{
 
   "Certificate generation process " should " not throw exception on disabled validation for signatorylist empty field values" in {
     val event = new Event(JSONUtil.deserialize[java.util.Map[String, Any]](EventFixture.EVENT_4), 0, 0)
-    noException should be thrownBy new CertValidator(jobConfig).validateGenerateCertRequest(event)
+    noException should be thrownBy new CertificateGeneratorFunction(jobConfig, httpUtil, storageService, cassandraUtil).processElement(event, null, mockMetrics)
   }
 
 }
