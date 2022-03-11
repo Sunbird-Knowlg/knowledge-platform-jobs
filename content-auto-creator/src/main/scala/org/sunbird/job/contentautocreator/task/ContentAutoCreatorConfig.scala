9c06dfb36e6cc4a0b464fb1b2b547164dad72213
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2022-03-11 13:59:00 +0530

    Issue #SB-29095 fix: Content-auto-creator job failing

--- content-auto-creator/src/main/scala/org/sunbird/job/contentautocreator/task/ContentAutoCreatorConfig.scala
+++ content-auto-creator/src/main/scala/org/sunbird/job/contentautocreator/task/ContentAutoCreatorConfig.scala
@@ -18,16 +18,18 @@ class ContentAutoCreatorConfig(override val config: Config) extends BaseJobConfi
 
   // Kafka Topics Configuration
   val kafkaInputTopic: String = config.getString("kafka.input.topic")
+  val kafkaFailedTopic: String = config.getString("kafka.failed.topic")
   override val kafkaConsumerParallelism: Int = config.getInt("task.consumer.parallelism")
   override val parallelism: Int = config.getInt("task.parallelism")
   val contentAutoCreatorParallelism: Int = if (config.hasPath("task.content-auto-creator.parallelism"))
     config.getInt("task.content-auto-creator.parallelism") else 1
 
   // Metric List
-  val totalEventsCount = "total-events-count"
-  val successEventCount = "success-events-count"
-  val failedEventCount = "failed-events-count"
-  val skippedEventCount = "skipped-events-count"
+  val totalEventsCount = "total-message-count"
+  val successEventCount = "success-message-count"
+  val failedEventCount = "failed-message-count"
+  val skippedEventCount = "skipped-message-count"
+  val errorEventCount = "error-message-count"
 
   // Consumers
   val eventConsumer = "content-auto-creator-consumer"
@@ -36,6 +38,7 @@ class ContentAutoCreatorConfig(override val config: Config) extends BaseJobConfi
 
   // Tags
   val contentAutoCreatorOutputTag: OutputTag[Event] = OutputTag[Event]("content-auto-creator")
+  val failedEventOutTag: OutputTag[String] = OutputTag[String]("failed-event")
 
   val configVersion = "1.0"
 
