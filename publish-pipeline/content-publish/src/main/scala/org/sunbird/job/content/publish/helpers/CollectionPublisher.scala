95117f404a05773afec95f484d9e0ce308d4045e
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2022-05-09 14:07:04 +0530

    Issue #SB-29873 fix: Collection Publish failing

--- publish-pipeline/content-publish/src/main/scala/org/sunbird/job/content/publish/helpers/CollectionPublisher.scala
+++ publish-pipeline/content-publish/src/main/scala/org/sunbird/job/content/publish/helpers/CollectionPublisher.scala
@@ -54,7 +54,8 @@ trait CollectionPublisher extends ObjectReader with SyncMessagesGenerator with O
 
   def getRelationalMetadata(identifier: String, pkgVersion: Double, readerConfig: ExtDataConfig)(implicit cassandraUtil: CassandraUtil): Option[Map[String, AnyRef]] = {
     val row: Row = Option(getCollectionHierarchy(getEditableObjId(identifier, pkgVersion), readerConfig)).getOrElse(getCollectionHierarchy(identifier, readerConfig))
-    if (null != row && row.getString("relational_metadata") != null) {
+    logger.info("CollectionPublisher:: enrichObjectMetadata:: getRelationalMetadata:: " + row.getString("relational_metadata"))
+    if (null != row && row.getString("relational_metadata") != null && row.getString("relational_metadata").nonEmpty) {
       val data: Map[String, AnyRef] = ScalaJsonUtil.deserialize[Map[String, AnyRef]](row.getString("relational_metadata"))
       Option(data)
     } else Option(Map.empty[String, AnyRef])
