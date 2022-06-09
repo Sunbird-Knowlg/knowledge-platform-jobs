03997ebb82953e7ec4e5acfc4384209d4633946f
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2022-06-09 14:40:17 +0530

    Issue #SB-30118 feat: DIAL context update by content service

--- dialcode-context-updater/src/main/scala/org/sunbird/job/dialcodecontextupdater/helpers/DialcodeContextUpdater.scala
+++ dialcode-context-updater/src/main/scala/org/sunbird/job/dialcodecontextupdater/helpers/DialcodeContextUpdater.scala
@@ -56,11 +56,11 @@ trait DialcodeContextUpdater {
 						record._2 match {
 							case stringValSubLevel: String =>
 								if (record._1.equalsIgnoreCase("@type")) (record._1 -> stringValSubLevel)
-								else if (!record._1.equalsIgnoreCase("@type") && finalFilteredData.contains(rec._1) && finalFilteredData(rec._1).isInstanceOf[String]) (record._1 -> finalFilteredData(rec._1).asInstanceOf[String])
-								else if (!record._1.equalsIgnoreCase("@type") && finalFilteredData.contains(rec._1) && finalFilteredData(rec._1).asInstanceOf[Map[String, AnyRef]].contains(record._1)) (record._1 -> finalFilteredData(rec._1).asInstanceOf[Map[String, AnyRef]](record._1))
+								else if (!record._1.equalsIgnoreCase("@type") && finalFilteredData.contains(rec._1) && finalFilteredData(rec._1).isInstanceOf[String]) (record._1 -> finalFilteredData(rec._1))
+								else if (!record._1.equalsIgnoreCase("@type") && finalFilteredData.contains(rec._1) && finalFilteredData(rec._1).asInstanceOf[Map[String, AnyRef]].contains(stringValSubLevel)) (record._1 -> finalFilteredData(rec._1).asInstanceOf[Map[String, AnyRef]](stringValSubLevel))
 								else (record._1 -> null)
 							case objectValSubLevel: Map[String, AnyRef] =>  (record._1 -> objectValSubLevel.map(l2Record => {
-									if(finalFilteredData(rec._1).asInstanceOf[Map[String, AnyRef]].contains(l2Record._1))
+									if(finalFilteredData.contains(rec._1) && finalFilteredData(rec._1).asInstanceOf[Map[String, AnyRef]].contains(l2Record._1))
 										(l2Record._1 -> finalFilteredData(rec._1).asInstanceOf[Map[String, AnyRef]](l2Record._1)) else (l2Record._1 -> null)
 							}).filter(checkRec => checkRec._2!=null))
 						}
