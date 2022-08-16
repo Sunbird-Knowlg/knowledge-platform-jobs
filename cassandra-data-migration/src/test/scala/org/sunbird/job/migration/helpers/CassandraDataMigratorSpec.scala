bd27de1a44402429a6fe3499d9b1094a8d2a2b99
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2022-08-16 16:03:56 +0530

    Issue #KN-195 feat: CSP: Blob path migration script for Cassandra 'hierarchy' data.

--- cassandra-data-migration/src/test/scala/org/sunbird/job/migration/helpers/CassandraDataMigratorSpec.scala
+++ cassandra-data-migration/src/test/scala/org/sunbird/job/migration/helpers/CassandraDataMigratorSpec.scala
@@ -42,11 +42,7 @@ class CassandraDataMigratorSpec extends BaseTestSpec {
     new TestCassandraDataMigrator().migrateData(jobConfig)(cassandraUtil)
     val row = new TestCassandraDataMigrator().readColumnDataFromCassandra(content_id, jobConfig)(cassandraUtil)
     val migratedData: String = row.getString(jobConfig.columnToMigrate)
-    println("migratedData:: " + migratedData)
-    println("jobConfig.keyValueMigrateStrings.values().toArray().head:: " + jobConfig.keyValueMigrateStrings.values().toArray().head)
-
-    println("migratedData.contains(jobConfig.keyValueMigrateStrings.values().toArray().head):: " + migratedData.contains("\""+jobConfig.keyValueMigrateStrings.values().toArray().head))
-    assert(migratedData.contains("\""+jobConfig.keyValueMigrateStrings.values().toArray().head))
+   assert(migratedData.contains("\""+jobConfig.keyValueMigrateStrings.values().toArray().head))
   }
 
   def testCassandraUtil(cassandraUtil: CassandraUtil): Unit = {
