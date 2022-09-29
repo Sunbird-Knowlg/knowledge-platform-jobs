53021e4426e96ce5fed565bc9e0c34bb88039d0e
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2022-09-29 11:17:06 +0530

    Issue #SB-31006 fix: Reverting 30566 patch

--- content-auto-creator/src/main/scala/org/sunbird/job/contentautocreator/helpers/ContentAutoCreator.scala
+++ content-auto-creator/src/main/scala/org/sunbird/job/contentautocreator/helpers/ContentAutoCreator.scala
@@ -107,11 +107,7 @@ trait ContentAutoCreator extends ContentCollectionUpdater {
 				case _ => logger.info("ContentAutoCreator :: process :: Event Skipped for operations (create, upload, publish) for: " + event.identifier + " | Content Stage : " + contentStage)
 			}
 
-			delay(config.publishDelay)
-			val updatedContentMetadata = neo4JUtil.getNodeProperties(internalId)
-			logger.info("ContentAutoCreator :: process :: updatedContentMetadata status : "  + updatedContentMetadata.get("status").asInstanceOf[String])
-			if(!updatedContentMetadata.get("status").asInstanceOf[String].equalsIgnoreCase("Failed") &&
-				event.collection.nonEmpty && (linkToCollection || contentStage.equalsIgnoreCase("na"))) {
+			if(event.collection.nonEmpty && (linkToCollection || contentStage.equalsIgnoreCase("na"))) {
 				linkCollection(internalId, event.collection)(config, httpUtil)
 			} else logger.info("ContentAutoCreator :: process :: Textbook Linking Skipped because received empty collection/textbookInfo for : " + event.identifier)
 
