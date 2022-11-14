5d3a882c45dd56a4a310a81cfcd5cebc62116fff
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2022-11-14 10:21:34 +0530

    Issue #KN-439 feat: CSP Migration Job

--- csp-migrator/src/main/scala/org/sunbird/job/cspmigrator/helpers/CSPCassandraMigrator.scala
+++ csp-migrator/src/main/scala/org/sunbird/job/cspmigrator/helpers/CSPCassandraMigrator.scala
@@ -1,12 +1,15 @@
 package org.sunbird.job.cspmigrator.helpers
 
 import com.datastax.driver.core.Row
+import org.apache.commons.io.{FileUtils, FilenameUtils}
 import org.apache.commons.lang3.StringUtils
 import org.slf4j.LoggerFactory
 import org.sunbird.job.cspmigrator.task.CSPMigratorConfig
 import org.sunbird.job.exception.ServerException
 import org.sunbird.job.util._
 
+import java.io.{File, IOException}
+import java.net.URL
 import scala.collection.JavaConverters._
 
 trait CSPCassandraMigrator extends MigrationObjectReader with MigrationObjectUpdater with URLExtractor {
@@ -35,7 +38,7 @@ trait CSPCassandraMigrator extends MigrationObjectReader with MigrationObjectUpd
 			val ecmlBody: String = getContentBody(identifier, config)(cassandraUtil)
 			logger.info(s"""CSPCassandraMigrator:: process:: $identifier - $objectType :: ECML Fetched body:: $ecmlBody""")
 
-			val migratedECMLBody: String = extractAndValidateUrls(ecmlBody, config, httpUtil, cloudStorageUtil)
+			val migratedECMLBody: String = extractAndValidateUrls(identifier, ecmlBody, config, httpUtil, cloudStorageUtil)
 
 			updateContentBody(identifier, migratedECMLBody, config)(cassandraUtil)
 			logger.info(s"""CSPCassandraMigrator:: process:: $identifier - $objectType :: ECML Migrated body:: $migratedECMLBody""")
@@ -45,7 +48,7 @@ trait CSPCassandraMigrator extends MigrationObjectReader with MigrationObjectUpd
 			status.equalsIgnoreCase("Unlisted")) && mimeType.equalsIgnoreCase("application/vnd.ekstep.content-collection")) {
 			val collectionHierarchy: String = getCollectionHierarchy(identifier, config)(cassandraUtil)
 			logger.info(s"""CSPCassandraMigrator:: process:: $identifier - $objectType :: Fetched Hierarchy:: $collectionHierarchy""")
-			val migratedCollectionHierarchy: String = extractAndValidateUrls(collectionHierarchy, config, httpUtil, cloudStorageUtil)
+			val migratedCollectionHierarchy: String = extractAndValidateUrls(identifier, collectionHierarchy, config, httpUtil, cloudStorageUtil)
 			updateCollectionHierarchy(identifier, migratedCollectionHierarchy, config)(cassandraUtil)
 			logger.info(s"""CSPCassandraMigrator:: process:: $identifier - $objectType :: Migrated Hierarchy:: $migratedCollectionHierarchy""")
 		}
@@ -53,19 +56,22 @@ trait CSPCassandraMigrator extends MigrationObjectReader with MigrationObjectUpd
 	}
 
 
-	def extractAndValidateUrls(contentString: String, config: CSPMigratorConfig, httpUtil: HttpUtil, cloudStorageUtil: CloudStorageUtil): String = {
+	def extractAndValidateUrls(identifier: String, contentString: String, config: CSPMigratorConfig, httpUtil: HttpUtil, cloudStorageUtil: CloudStorageUtil): String = {
 		val extractedUrls: List[String] = extarctUrls(contentString)
 
 		if(extractedUrls.nonEmpty) {
-			extractedUrls.toSet.foreach(url => {
+			extractedUrls.toSet.foreach(urlString => {
 				config.keyValueMigrateStrings.keySet().toArray().map(migrateDomain => {
-					if(url.contains(migrateDomain)) {
-						val migrateValue: String = StringUtils.replaceEach(url, config.keyValueMigrateStrings.keySet().toArray().map(_.asInstanceOf[String]), config.keyValueMigrateStrings.values().toArray().map(_.asInstanceOf[String]))
+					if(urlString.contains(migrateDomain)) {
+						val migrateValue: String = StringUtils.replaceEach(urlString, config.keyValueMigrateStrings.keySet().toArray().map(_.asInstanceOf[String]), config.keyValueMigrateStrings.values().toArray().map(_.asInstanceOf[String]))
 						if(httpUtil.getSize(migrateValue) < 0) {
-//							if (config.copyMissingFiles)
-//								clouds
-//							else
-														throw new ServerException("ERR_NEW_PATH_NOT_FOUND", "File not found in the new path to migrate: " + migrateValue)
+							if (config.copyMissingFiles) {
+								// code to download file from old cloud path and upload to new cloud path
+								val downloadedFile: File = downloadFile(s"/tmp/$identifier", urlString)
+								val folderName: String = ""
+								cloudStorageUtil.uploadFile(folderName,downloadedFile)
+
+							} else throw new ServerException("ERR_NEW_PATH_NOT_FOUND", "File not found in the new path to migrate: " + migrateValue)
 						}
 					}
 				})
@@ -73,5 +79,24 @@ trait CSPCassandraMigrator extends MigrationObjectReader with MigrationObjectUpd
 			StringUtils.replaceEach(contentString, config.keyValueMigrateStrings.keySet().toArray().map(_.asInstanceOf[String]), config.keyValueMigrateStrings.values().toArray().map(_.asInstanceOf[String]))
 		} else contentString
 	}
+
+
+	def downloadFile(downloadPath: String, fileUrl: String): File = try {
+		createDirectory(downloadPath)
+		val file = new File(downloadPath + File.separator + FilenameUtils.getName(fileUrl))
+		FileUtils.copyURLToFile(new URL(fileUrl), file)
+		file
+	} catch {
+		case e: IOException =>
+			e.printStackTrace()
+			throw new ServerException("ERR_INVALID_FILE_URL", "File not found in the old path to migrate: " + downloadPath)
+	}
+
+	def createDirectory(directoryName: String): Unit = {
+		val theDir = new File(directoryName)
+		if (!theDir.exists) theDir.mkdirs
+	}
+
+
 
 }
