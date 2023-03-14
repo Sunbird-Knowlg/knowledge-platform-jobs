74fb3be38098fed07aed7f4948b71353ca21a4b8
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2023-03-14 16:06:09 +0530

    Issue #KN-802 feat: Sync QR Image to DIAL code ES index

--- qrcode-image-generator/src/main/scala/org/sunbird/job/qrimagegenerator/util/QRCodeImageGeneratorUtil.scala
+++ qrcode-image-generator/src/main/scala/org/sunbird/job/qrimagegenerator/util/QRCodeImageGeneratorUtil.scala
@@ -271,7 +271,8 @@ class QRCodeImageGeneratorUtil(config: QRCodeImageGeneratorConfig, cassandraUtil
     logger.info("QRCodeImageGeneratorUtil::indexImageInDocument:: indexDocument:: " + indexDocument)
     if(indexDocument!=null && indexDocument.nonEmpty && !indexDocument.contains("url")) {
       val query = QueryBuilder.select("url").from(config.cassandraKeyspace, config.cassandraDialCodeImageTable)
-        .where(QueryBuilder.eq("filename", indexDocument.get("filename").toString))
+        .allowFiltering()
+        .where(QueryBuilder.eq("dialcode", indexDocument.get("identifier").toString))
       logger.info("QRCodeImageGeneratorUtil::indexImageInDocument:: query:: " + query)
       val row: Row = cassandraUtil.findOne(query.toString)
       if(null != row && !row.isNull("url")) {
