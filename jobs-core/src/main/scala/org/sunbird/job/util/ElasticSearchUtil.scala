ca8013b516416e84563d72708473d53a0443ef84
aimansharief <aiman.sharief.17@gmail.com>
2024-02-29 11:20:45 +0530

    Issue #KN-976 fix: Updated Elasticsearch version, settings, mappings and requests

--- jobs-core/src/main/scala/org/sunbird/job/util/ElasticSearchUtil.scala
+++ jobs-core/src/main/scala/org/sunbird/job/util/ElasticSearchUtil.scala
@@ -14,9 +14,9 @@ import org.elasticsearch.action.delete.DeleteRequest
 import org.elasticsearch.action.get.GetRequest
 import org.elasticsearch.action.index.IndexRequest
 import org.elasticsearch.action.update.UpdateRequest
-import org.elasticsearch.client.{Response, RestClient, RestClientBuilder, RestHighLevelClient}
+import org.elasticsearch.client.{Request, RequestOptions, Response, RestClient, RestClientBuilder, RestHighLevelClient}
 import org.elasticsearch.common.settings.Settings
-import org.elasticsearch.common.xcontent.XContentType
+import org.elasticsearch.xcontent.XContentType
 import org.slf4j.LoggerFactory
 
 import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
@@ -49,7 +49,7 @@ class ElasticSearchUtil(connectionInfo: String, indexName: String, indexType: St
 
   def isIndexExists(): Boolean = {
     try {
-      val response = esClient.getLowLevelClient.performRequest("HEAD", "/" + indexName)
+      val response = esClient.getLowLevelClient.performRequest(new Request("HEAD", "/" + indexName))
       response.getStatusLine.getStatusCode == 200
     } catch {
       case e: IOException => {
@@ -67,7 +67,7 @@ class ElasticSearchUtil(connectionInfo: String, indexName: String, indexType: St
       if (StringUtils.isNotBlank(alias)) createRequest.alias(new Alias(alias))
       if (StringUtils.isNotBlank(settings)) createRequest.settings(Settings.builder.loadFromSource(settings, XContentType.JSON))
       if (StringUtils.isNotBlank(indexType) && StringUtils.isNotBlank(mappings)) createRequest.mapping(indexType, mappings, XContentType.JSON)
-      val createIndexResponse = client.indices.create(createRequest)
+      val createIndexResponse = client.indices.create(createRequest, RequestOptions.DEFAULT)
       response = createIndexResponse.isAcknowledged
     }
     response
@@ -79,7 +79,7 @@ class ElasticSearchUtil(connectionInfo: String, indexName: String, indexType: St
       // Replace mapper with JSONUtil once the JSONUtil is fixed
       val doc = mapper.readValue(document, new TypeReference[util.Map[String, AnyRef]]() {})
       val updatedDoc = checkDocStringLength(doc)
-      val response = esClient.index(new IndexRequest(indexName, indexType, identifier).source(updatedDoc))
+      val response = esClient.index(new IndexRequest(indexName).id(identifier).source(updatedDoc), RequestOptions.DEFAULT)
       logger.info(s"Added ${response.getId} to index ${response.getIndex}")
     } catch {
       case e: IOException =>
@@ -95,7 +95,7 @@ class ElasticSearchUtil(connectionInfo: String, indexName: String, indexType: St
       val doc = mapper.readValue(document, new TypeReference[util.Map[String, AnyRef]]() {})
       val updatedDoc = checkDocStringLength(doc)
       val indexRequest = if(identifier == null) new IndexRequest(indexName, indexType) else new IndexRequest(indexName, indexType, identifier)
-      val response = esClient.index(indexRequest.source(updatedDoc))
+      val response = esClient.index(indexRequest.source(updatedDoc), RequestOptions.DEFAULT)
       logger.info(s"Added ${response.getId} to index ${response.getIndex}")
     } catch {
       case e: IOException =>
@@ -113,7 +113,7 @@ class ElasticSearchUtil(connectionInfo: String, indexName: String, indexType: St
       val updatedDoc = checkDocStringLength(doc)
       val indexRequest = new IndexRequest(indexName, indexType, identifier).source(updatedDoc)
       val request = new UpdateRequest().index(indexName).`type`(indexType).id(identifier).doc(updatedDoc).upsert(indexRequest)
-      val response = esClient.update(request)
+      val response = esClient.update(request, RequestOptions.DEFAULT)
       logger.info(s"Updated ${response.getId} to index ${response.getIndex}")
     } catch {
       case e: IOException =>
@@ -122,12 +122,12 @@ class ElasticSearchUtil(connectionInfo: String, indexName: String, indexType: St
   }
 
   def deleteDocument(identifier: String): Unit = {
-    val response = esClient.delete(new DeleteRequest(indexName, indexType, identifier))
+    val response = esClient.delete(new DeleteRequest(indexName, indexType, identifier),RequestOptions.DEFAULT)
     logger.info(s"Deleted ${response.getId} to index ${response.getIndex}")
   }
 
   def getDocumentAsString(identifier: String): String = {
-    val response = esClient.get(new GetRequest(indexName, indexType, identifier))
+    val response = esClient.get(new GetRequest(indexName, indexType, identifier), RequestOptions.DEFAULT)
     response.getSourceAsString
   }
 
@@ -154,7 +154,7 @@ class ElasticSearchUtil(connectionInfo: String, indexName: String, indexType: St
           logger.debug("ElasticSearchUtil:: bulkIndexWithIndexId:: doc: " + updatedDoc)
           request.add(new IndexRequest(indexName, documentType, key).source(updatedDoc))
           if (count % batchSize == 0 || (count % batchSize < batchSize && count == jsonObjects.size)) {
-            val bulkResponse = esClient.bulk(request)
+            val bulkResponse = esClient.bulk(request, RequestOptions.DEFAULT)
             if (bulkResponse.hasFailures) logger.info("ElasticSearchUtil:: bulkIndexWithIndexId:: Failures in Elasticsearch bulkIndex : " + bulkResponse.buildFailureMessage)
           }
         }
@@ -165,7 +165,7 @@ class ElasticSearchUtil(connectionInfo: String, indexName: String, indexType: St
 
   def isIndexExists(indexName: String): Boolean = {
     try {
-      val response: Response = esClient.getLowLevelClient.performRequest("HEAD", "/" + indexName)
+      val response: Response = esClient.getLowLevelClient.performRequest(new Request("HEAD", "/" + indexName))
       response.getStatusLine.getStatusCode == 200
     } catch {
       case e: IOException =>  false
